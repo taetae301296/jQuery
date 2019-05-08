a64073d96bf5dd1102bf00e8de88cc6c2a4c906c
Hiep Dam Thi <intern.dthihiep@tma.com.vn>
2019-05-08 17:27:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(
+    // function(){
+    //     $('.img-car').hover(
+    //         function(){
+    //             // alert('hello');
+    //             $('.info-ourwork').addClass('info-ourwork-title');
+    //         }
+    //     )
+    // }
+
+    // function(){
+    //     $('.img-car').click(
+    //         function(){
+    //             $(window).load(
+    //                 function(){
+    //                     $('.flexslider').flexslider({
+    //                         animation: "slide"
+    //                     });
+    //                 }
+    //             )
+    //         }
+    //     )
+    // }
+)
+
+
+// Can also be used with $(document).ready()
+// $(window).load(function() {
+//     $('.flexslider').flexslider({
+//       animation: "slide"
+//     });
+// });
